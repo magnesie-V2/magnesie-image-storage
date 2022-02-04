a6ecf4d9456c743bda1ac49d9cfc3e0d1d055da9
Mathieu MÉRILLON <mathieumeri@hotmail.fr>
2022-02-05 00:36:23 +0100

    Fusion of orchestrator and storages components in one docker-compose

--- database/0.0.0-00_init.sql
+++ database/0.0.0-00_init.sql
@@ -2,8 +2,18 @@ DROP SCHEMA IF EXISTS `magnesie_image_storage`;
 
 CREATE SCHEMA IF NOT EXISTS `magnesie_image_storage`;
 
+DROP SCHEMA IF EXISTS `magnesie_result_storage`;
+
+CREATE SCHEMA IF NOT EXISTS `magnesie_result_storage`;
+
+
 CREATE USER 'magnesie_image_storage'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
 
 GRANT ALL PRIVILEGES ON `magnesie_image_storage`.* TO 'magnesie_image_storage'@'%';
+
+CREATE USER 'magnesie_result_storage'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
+
+GRANT ALL PRIVILEGES ON `magnesie_result_storage`.* TO 'magnesie_result_storage'@'%';
+
 
 FLUSH PRIVILEGES;
--- database/0.1.0-01_table_creation.sql
+++ database/0.1.0-01_table_creation.sql
@@ -1,17 +1,17 @@
-USE `magnesie_image_storage`;
+USE 'magnesie_image_storage';
 
-CREATE TABLE IF NOT EXISTS `submissions` (
-    `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `submission_date` TIMESTAMP NOT NULL,
-    `status` VARCHAR(10) NOT NULL,
+CREATE TABLE IF NOT EXISTS 'submissions' (
+    'id' INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    'submission_date' TIMESTAMP NOT NULL,
+    'status' VARCHAR(10) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS `photos` (
-    `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `file_name` VARCHAR(100) NOT NULL,
-    `submission_id` INT NOT NULL,
-    `path` VARCHAR(100) NOT NULL,
-    FOREIGN KEY (`submission_id`) REFERENCES `submissions`(`id`) ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS 'photos' (
+    'id' INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    'file_name' VARCHAR(100) NOT NULL,
+    'submission_id' INT NOT NULL,
+    'path' VARCHAR(100) NOT NULL,
+    FOREIGN KEY ('submission_id') REFERENCES 'submissions'('id') ON DELETE CASCADE
 );
 
 COMMIT;
