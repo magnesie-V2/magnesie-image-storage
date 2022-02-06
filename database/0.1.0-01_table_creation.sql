8a5dbb58931ac43c078ec0ab1bbf68bbe57c799c
Mathieu MÉRILLON <mathieumeri@hotmail.fr>
2022-02-06 19:42:26 +0100

    Correct SQL script

--- database/0.1.0-01_table_creation.sql
+++ database/0.1.0-01_table_creation.sql
@@ -1,17 +1,27 @@
-USE 'magnesie_image_storage';
+USE magnesie_image_storage;
 
-CREATE TABLE IF NOT EXISTS 'submissions' (
-    'id' INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    'submission_date' TIMESTAMP NOT NULL,
-    'status' VARCHAR(10) NOT NULL
+CREATE TABLE IF NOT EXISTS submissions (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    submission_date TIMESTAMP NOT NULL,
+    status VARCHAR(10) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS 'photos' (
-    'id' INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    'file_name' VARCHAR(100) NOT NULL,
-    'submission_id' INT NOT NULL,
-    'path' VARCHAR(100) NOT NULL,
-    FOREIGN KEY ('submission_id') REFERENCES 'submissions'('id') ON DELETE CASCADE
+CREATE TABLE IF NOT EXISTS photos (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    file_name VARCHAR(100) NOT NULL,
+    submission_id INT NOT NULL,
+    path VARCHAR(100) NOT NULL,
+    FOREIGN KEY (submission_id) REFERENCES submissions(id) ON DELETE CASCADE
+);
+
+COMMIT;
+
+USE magnesie_result_storage;
+
+CREATE TABLE IF NOT EXISTS results (
+    id INT PRIMARY KEY NOT NULL,
+    model_path VARCHAR(200) NOT NULL,
+    texture_path VARCHAR(200) NOT NULL
 );
 
 COMMIT;
